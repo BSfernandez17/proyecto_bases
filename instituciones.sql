79513760f496ffdbf5ce804b70826da0b11b32d3
BSfernandez17 <brayanfernandez1706@gmail.com>
2023-05-27 11:59:01 -0500

    todas las tablas del modelo ER

--- instituciones.sql
+++ instituciones.sql
@@ -37,7 +37,7 @@ nomb_nombram varchar (50));
 CREATE TABLE sectores (cod_sector serial PRIMARY KEY,
 nomb_sector varchar(50));
 
-CREATE TABLE Caracter_academico(cod_academ serial PRIMARY KEY,
+CREATE TABLE Caracter\_academico(cod_academ serial PRIMARY KEY,
 nomb_academ varchar(20));
 
 CREATE TABLE Instituciones(cod_inst serial PRIMARY KEY,
@@ -46,3 +46,46 @@ cod_academ INT,
 cod_sector INT,
 CONSTRAINT fk_cod_academ FOREIGN KEY (cod_academ) REFERENCES Caracter_academico(cod_academ),
 CONSTRAINT fk_cod_sector FOREIGN KEY (cod_sector) REFERENCES sectores(cod_sector));
+
+
+CREATE TABLE Inst_por_municipio(cod_inst_munic varchar(5) PRIMARY KEY,
+    direccion varchar(50),
+	telefono varchar(50),
+	norma varchar(50),
+	fecha_creacion DATE,
+	programas_vigente int NOT NULL CHECK(programas_vigente>=0),
+	acreditada varchar(1),
+	fecha_acreditacion DATE,
+	resolucion_acreditacion varchar(50),
+	vigencia int CHECK(vigencia>0),
+	nit varchar(50),
+	pagina_web varchar(50),
+
+
+	
+	cod_juridica INT,
+	cod_seccional INT,
+	cod_admon INT,
+	cod_norma INT,
+	cod_estado int,
+    cod_munic INT,
+    CONSTRAINT fk_juridica FOREIGN KEY (cod_juridica) REFERENCES Naturaleza_juridica(cod_juridica),
+    CONSTRAINT fk_cod_seccional FOREIGN KEY (cod_seccional) REFERENCES seccional(cod_seccional),
+    CONSTRAINT fk_cod_admon FOREIGN KEY (cod_admon) REFERENCES Acto_admon(cod_admon),
+    CONSTRAINT fk_cod_norma FOREIGN KEY (cod_norma) REFERENCES Norma_creacion(cod_norma),
+    CONSTRAINT fk_cod_estado FOREIGN KEY (cod_estado) REFERENCES Estados(cod_estado),
+    CONSTRAINT fk_cod_munic FOREIGN KEY (cod_munic) REFERENCES Municipios(cod_munic)
+   );
+
+CREATE TABLE Rectoria(fecha_inicio DATE,
+fecha_final DATE,
+cod_cargo INT,
+cod_nomb INT,
+cod_munic INT,
+cod_inst INT,
+cod_directivo INT
+);
+ALTER TABLE rectoria ADD CONSTRAINT pk_cod_rectoria PRIMARY KEY(cod_cargo,cod_nomb,cod_inst);
+ALTER TABLE rectoria ADD CONSTRAINT fk_cod_cargo FOREIGN KEY(cod_cargo) REFERENCES cargos(cod_cargo);
+ALTER TABLE rectoria ADD CONSTRAINT fk_cod_nomb FOREIGN KEY(cod_nomb) REFERENCES acto_nombramiento(cod_nombram);
+ALTER TABLE rectoria ADD CONSTRAINT fk_cod_directivo FOREIGN KEY(cod_directivo) REFERENCES Directivos(cod_directivo);

6f346db961aaa30f8057c94450640b1b1b431e58
BSfernandez17 <brayanfernandez1706@gmail.com>
2023-06-21 18:06:59 -0500

    llaves primarias como varchar

--- instituciones.sql
+++ instituciones.sql
@@ -1,46 +1,43 @@
-CREATE TABLE sectores(cod_sector serial PRIMARY KEY,
+CREATE TABLE sectores(cod_sector varchar(15) PRIMARY KEY,
 nomb_sector varchar(80));
 
-CREATE TABLE Departamentos(cod_depto serial PRIMARY KEY, 
+CREATE TABLE Departamentos(cod_depto varchar(15) PRIMARY KEY, 
 nomb_depto varchar(50));
 
-CREATE TABLE Municipios(cod_munic serial PRIMARY KEY, 
+CREATE TABLE Municipios(cod_munic varchar(15) PRIMARY KEY, 
 nomb_munic varchar(50),
 cod_depto INT,
 CONSTRAINT fk_cod_depto FOREIGN KEY (cod_depto) REFERENCES Departamentos(cod_depto));
 
-CREATE TABLE Norma_creacion(cod_norma serial PRIMARY KEY,
+CREATE TABLE Norma_creacion(cod_norma varchar(15)  PRIMARY KEY,
 nomb_norma varchar(100));
 
-CREATE TABLE Acto_admon(cod_admon serial PRIMARY KEY,
+CREATE TABLE Acto_admon(cod_admon varchar(15)  PRIMARY KEY,
 nomb_admon varchar (80));
 
-CREATE TABLE seccional(cod_seccional serial PRIMARY KEY,
+CREATE TABLE seccional(cod_seccional varchar(15)  PRIMARY KEY,
 nomb_seccional varchar (20));
 
 CREATE TABLE Naturaleza_juridica(cod_juridica serial PRIMARY KEY,
 nomb_juridica varchar(80));
 
-CREATE TABLE Estados(cod_estado serial PRIMARY KEY,
+CREATE TABLE Estados(cod_estado varchar(15)  PRIMARY KEY,
 nomb_estado varchar(20));
 
 
-CREATE TABLE Directivos(cod_directivo serial PRIMARY KEY,
+CREATE TABLE Directivos(cod_directivo varchar(15)  PRIMARY KEY,
 nomb_directivo varchar(50));
 
-CREATE TABLE Cargos(cod_cargo serial PRIMARY KEY,
+CREATE TABLE Cargos(cod_cargo varchar(15)  PRIMARY KEY,
 nomb_cargo varchar(50));
 
-CREATE TABLE Acto_nombramiento(cod_nombram serial PRIMARY KEY,
+CREATE TABLE Acto_nombramiento(cod_nombram varchar(15)  PRIMARY KEY,
 nomb_nombram varchar (50));
 
-CREATE TABLE sectores (cod_sector serial PRIMARY KEY,
-nomb_sector varchar(50));
-
-CREATE TABLE Caracter\_academico(cod_academ serial PRIMARY KEY,
+CREATE TABLE Caracter\_academico(cod_academ varchar(15)  PRIMARY KEY,
 nomb_academ varchar(20));
 
-CREATE TABLE Instituciones(cod_inst serial PRIMARY KEY,
+CREATE TABLE Instituciones(cod_inst varchar(15)  PRIMARY KEY,
 nomb_inst varchar(50),
 cod_academ INT,
 cod_sector INT,
@@ -48,7 +45,7 @@ CONSTRAINT fk_cod_academ FOREIGN KEY (cod_academ) REFERENCES Caracter_academico(
 CONSTRAINT fk_cod_sector FOREIGN KEY (cod_sector) REFERENCES sectores(cod_sector));
 
 
-CREATE TABLE Inst_por_municipio(cod_inst_munic varchar(5) PRIMARY KEY,
+CREATE TABLE Inst_por_municipio(
     direccion varchar(50),
 	telefono varchar(50),
 	norma varchar(50),
@@ -60,30 +57,32 @@ CREATE TABLE Inst_por_municipio(cod_inst_munic varchar(5) PRIMARY KEY,
 	vigencia int CHECK(vigencia>0),
 	nit varchar(50),
 	pagina_web varchar(50),
-
-
+	cod_inst_por_municipio varchar(50),
+	cod_inst varchar(15) ,
 	
-	cod_juridica INT,
-	cod_seccional INT,
-	cod_admon INT,
-	cod_norma INT,
+	cod_juridica varchar(15) ,
+	cod_seccional varchar(15) ,
+	cod_admon varchar(15) ,
+	cod_norma varchar(15) ,
 	cod_estado int,
-    cod_munic INT,
+    cod_munic varchar(15) ,
+	CONSTRAINT fk_cod_inst FOREIGN KEY (cod_inst) REFERENCES Instituciones(cod_inst),
     CONSTRAINT fk_juridica FOREIGN KEY (cod_juridica) REFERENCES Naturaleza_juridica(cod_juridica),
     CONSTRAINT fk_cod_seccional FOREIGN KEY (cod_seccional) REFERENCES seccional(cod_seccional),
     CONSTRAINT fk_cod_admon FOREIGN KEY (cod_admon) REFERENCES Acto_admon(cod_admon),
     CONSTRAINT fk_cod_norma FOREIGN KEY (cod_norma) REFERENCES Norma_creacion(cod_norma),
     CONSTRAINT fk_cod_estado FOREIGN KEY (cod_estado) REFERENCES Estados(cod_estado),
-    CONSTRAINT fk_cod_munic FOREIGN KEY (cod_munic) REFERENCES Municipios(cod_munic)
+    CONSTRAINT fk_cod_munic FOREIGN KEY (cod_munic) REFERENCES Municipios(cod_munic),
+	CONSTRAINT pk_cod_inst PRIMARY KEY (cod_inst,cod_munic)
    );
 
 CREATE TABLE Rectoria(fecha_inicio DATE,
 fecha_final DATE,
-cod_cargo INT,
-cod_nomb INT,
-cod_munic INT,
-cod_inst INT,
-cod_directivo INT
+cod_cargo varchar(15) ,
+cod_nomb varchar(15) ,
+cod_munic varchar(15) ,
+cod_inst varchar(15) ,
+cod_directivo varchar(15) 
 );
 ALTER TABLE rectoria ADD CONSTRAINT pk_cod_rectoria PRIMARY KEY(cod_cargo,cod_nomb,cod_inst);
 ALTER TABLE rectoria ADD CONSTRAINT fk_cod_cargo FOREIGN KEY(cod_cargo) REFERENCES cargos(cod_cargo);
